bddaf96a42a1e1c89ae9c501bfdd99beb1c5e555
Penpen7 <Raosys@live.jp>
2024-03-23 14:36:16 +0900

    lazyに変更

--- workspaces/app/src/foundation/components/Image.tsx
+++ workspaces/app/src/foundation/components/Image.tsx
@@ -1,16 +1,16 @@
 import type * as CSS from 'csstype';
 import styled from 'styled-components';
 
-import { addUnitIfNeeded } from '../../lib/css/addUnitIfNeeded';
+import {addUnitIfNeeded} from '../../lib/css/addUnitIfNeeded';
 
 const _Image = styled.img<{
   $height: number | string;
   $objectFit: string;
   $width: number | string;
 }>`
-  object-fit: ${({ $objectFit }) => $objectFit};
-  width: ${({ $width }) => addUnitIfNeeded($width)};
-  height: ${({ $height }) => addUnitIfNeeded($height)};
+  object-fit: ${({$objectFit}) => $objectFit};
+  width: ${({$width}) => addUnitIfNeeded($width)};
+  height: ${({$height}) => addUnitIfNeeded($height)};
   display: block;
 `;
 
@@ -20,6 +20,6 @@ type Props = {
   width: number | string;
 } & JSX.IntrinsicElements['img'];
 
-export const Image: React.FC<Props> = ({ height, loading = 'eager', objectFit, width, ...rest }) => {
+export const Image: React.FC<Props> = ({height, loading = 'lazy', objectFit, width, ...rest}) => {
   return <_Image {...rest} $height={height} $objectFit={objectFit} $width={width} loading={loading} />;
 };
