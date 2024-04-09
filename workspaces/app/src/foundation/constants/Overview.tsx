8a385e0248e842e105ce7d4d734a2dae85f239e3
Penpen7 <Raosys@live.jp>
2024-04-10 02:34:30 +0900

    定数を外出し

---
+++ workspaces/app/src/foundation/constants/Overview.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Overview() {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    fetch('/assets/overview.txt') // テキストファイルのパスを指定
+      .then((response) => response.text())
+      .catch((error) => {
+        console.error('Error fetching text file:', error);
+        return ''; // エラー時は空の文字列を返す
+      })
+      .then((text) => {
+        setContent(text);
+      });
+  }, []);
+
+  return <React.Fragment>{content}</React.Fragment>;
+}
