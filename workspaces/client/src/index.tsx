18d8d406752648dc432d8a3bf0d67acb3ae05705
Penpen7 <Raosys@live.jp>
2024-04-11 01:32:03 +0900

    jqueryの削除

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,6 +1,3 @@
-// import './side-effects';
-
-import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
@@ -8,19 +5,17 @@ import { SWRConfig } from 'swr';
 import { AdminApp } from '@wsh-2024/admin/src/index';
 import { ClientApp } from '@wsh-2024/app/src/index';
 
-import { preloadImages } from './utils/preloadImages';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
 const main = async () => {
-  await registerServiceWorker();
-  // await preloadImages();
+  registerServiceWorker();
 
-  $(document).ready(() => {
+  document.addEventListener('DOMContentLoaded', () => {
     if (window.location.pathname.startsWith('/admin')) {
-      ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
+      ReactDOM.createRoot(document.getElementById('root')!).render(<AdminApp />);
     } else {
       ReactDOM.hydrateRoot(
-        $('#root').get(0)!,
+        document.getElementById('root')!,
         <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
           <BrowserRouter>
             <ClientApp />
